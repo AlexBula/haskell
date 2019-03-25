caf5cb936bbe77a2a041a52409f0e95b1d557fc8
Aleksander Buła <bula.aleksander@gmail.com>
2019-03-25 12:02:57 +0100

    Implementation of part B

--- task_1/Main.hs
+++ task_1/Main.hs
@@ -2,14 +2,32 @@ module Main(main) where
 
 import System.Environment 
 import Text.Read
--- import Data.Scientific(isInteger)
+import Lib
+import Mon
+
+instance Mon Picture where
+  m1 = PP []
+  (><) p p1 = (&) p p1 
+
+parse :: Picture
+parse = PP [((P 0 0), (P 2 2))]
+
+
+runParser :: Maybe Int -> [IntLine]
+runParser (Just scalar) = renderScaled scalar parse
+
+drawPicture :: [IntLine] -> String
+drawPicture x = "300 400 translate\n\n" ++ foldr f "" x ++ "\nstroke showpage\n" where 
+  f ((x, y), (x1, y1)) acc = show x ++ " " ++ show y ++ " moveto " ++ show x1 ++ " " ++ show y1 ++ " lineto\n"
 
 main = do
     args <- System.Environment.getArgs
-    let first = head args
-    let scalar = readMaybe first :: Maybe Int
-    if scalar == Nothing then
-      print "Wrong argument"
+    if args == [] then
+      putStr $ drawPicture $ runParser $ Just 1
     else
-      print scalar
+      let scalar = readMaybe (head args) :: Maybe Int in
+        if scalar == Nothing || length args /= 1 then
+          putStr "Error. Usage: runhaskell Main.hs scalar_int(optional)\n"
+        else
+          putStr $ drawPicture $ runParser $ scalar
     
